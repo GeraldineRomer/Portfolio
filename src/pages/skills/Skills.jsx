e5a6549431953b5f69574703d1feb54d3331d424
GeraldineRomer <geraldine.rome0104@gmail.com>
2023-07-26 21:38:47 -0500

    first dropdown

--- src/pages/skills/Skills.jsx
+++ src/pages/skills/Skills.jsx
@@ -1,6 +1,30 @@
-import { Row } from 'antd';
+import { Col, Dropdown, Row, Space } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
 import './Skills.scss';
 import React from 'react';
+
+const items = [
+    {
+        key: '1',
+        label: 'HTML'
+    },
+    {
+        key: '2',
+        label: 'SCSS'
+    },
+    {
+        key: '3',
+        label: 'JavaScript'
+    },
+    {
+        key: '4',
+        label: 'React JS',
+    },
+    {
+        key: '5',
+        label: 'Bootstrap'
+    }
+];
 
 const Skills = () => {
     return (
@@ -8,6 +32,26 @@ const Skills = () => {
             <div>
                 <label className='skills-title'>Skills</label>
                 <label className='skills-subtitle'>My technical level</label>
+            </div>
+            <div>
+                <Row>
+                    <Col md={8}>
+                        <Dropdown
+                            menu={{
+                                items,
+                            }}
+                            className='skills-dropdown1'
+                        >
+                            <a onClick={(e) => e.preventDefault()}>
+                                <Space>
+                                    <i class="bi bi-code-slash skills-dropdown1-devicon"></i>
+                                    <label className='skills-dropdown1-label'>Frontend developer</label>
+                                    <DownOutlined className='skills-dropdown1-drownicon'/>
+                                </Space>
+                            </a>
+                        </Dropdown>
+                    </Col>
+                </Row>
             </div>
         </div>
     );
